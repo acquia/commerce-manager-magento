8cec79f5c1229d09f4954ed7a4c607802b999472
nikunj <me@nik4u.com>
2018-06-28 11:27:00 +0530

    Add new API to return Product Attributes with Swatches data.

---
+++ Model/Entity/Attribute.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Acquia/CommerceManager/Model/Product/Attribute/OptionManagement.php
+ *
+ * Product Attribute Options with Swatches data.
+ *
+ * All rights reserved. No unauthorized distribution or reproduction.
+ */
+
+namespace Acquia\CommerceManager\Model\Entity;
+
+use Acquia\CommerceManager\Api\Data\AttributeInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Api\AttributeValueFactory;
+use Magento\Framework\Stdlib\DateTime\DateTimeFormatterInterface;
+use Magento\Framework\App\ObjectManager;
+
+/**
+ * EAV Entity attribute model
+ *
+ * @method \Magento\Eav\Model\Entity\Attribute setOption($value)
+ * @method \Magento\Eav\Api\Data\AttributeExtensionInterface getExtensionAttributes()
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class Attribute extends \Magento\Catalog\Model\ResourceModel\Eav\Attribute implements \Acquia\CommerceManager\Api\Data\AttributeInterface
+{
+
+    private $swatches = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSwatches(array $swatches = [])
+    {
+        $this->swatches = $swatches;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSwatches()
+    {
+        return $this->swatches;
+    }
+}
