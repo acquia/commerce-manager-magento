2dea0c5c3207dff8a58b5510c43c7d9e761ca528
malachy <malachy.mcconnell@acquia.com>
2018-08-14 12:28:31 -0400

    Make getRelatedProducts extensible

--- Model/TargetRuleRepository.php
+++ Model/TargetRuleRepository.php
@@ -10,9 +10,8 @@
 
 namespace Acquia\CommerceManager\Model;
 
-use Acquia\CommerceManager\Api\Data\TargetRuleProductsInterface;
+use Acquia\CommerceManager\Model\TargetRuleProductsFactory as TargetRuleProductsFactory;
 use Acquia\CommerceManager\Api\TargetRuleRepositoryInterface;
-use Acquia\CommerceManager\Model\TargetRuleProducts;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Model\Product\Visibility;
@@ -72,78 +71,166 @@ class TargetRuleRepository implements TargetRuleRepositoryInterface
     private $targetRuleIndex;
 
     /**
+     * @var \Magento\TargetRule\Model\Rule
+     */
+    private $targetRuleClass;
+
+    /**
      * Product Visibility Model
      * @var Visibility $visibility
      */
     private $visibility;
 
     /**
+     * @var TargetRuleProductsFactory
+     */
+    private $targetRuleProductsFactory;
+
+    /**
+     * @var $targetRuleProducts \Acquia\CommerceManager\Model\TargetRuleProducts
+     */
+    public $relatedProducts;
+
+    /**
      * Constructor
      *
+     * @param TargetRuleProductsFactory $targetRuleProductsFactory
      * @param ModuleListInterface $moduleList
+     * @param ProductRepositoryInterface $productRepository
+     * @param CollectionFactory $productCollectionFactory
+     * @param Visibility $visibility
      */
     public function __construct(
+        TargetRuleProductsFactory $targetRuleProductsFactory,
         ModuleListInterface $moduleList,
         ProductRepositoryInterface $productRepository,
         CollectionFactory $productCollectionFactory,
         Visibility $visibility
     ) {
+        $this->targetRuleProductsFactory = $targetRuleProductsFactory;
         $this->moduleList = $moduleList;
         $this->productRepository = $productRepository;
         $this->productCollectionFactory = $productCollectionFactory;
         $this->visibility = $visibility;
     }
 
+
+    /**
+     * getProductsByType
+     *
+     * Returns lists of product relations by type and product sku.
+     *
+     * Public method here allows others to plugin around this method *but*
+     * please add your custom relation types under extension_attributes
+     * if you want them to be processed by ACM Commerce Connector so the
+     * returned data object might look like this:
+     * {
+     *     "crosssell": [
+     *         "sku1",
+     *         "sku2"
+     *     ],
+     *     "related": [
+     *         "sku3",
+     *         "sku4"
+     *     ],
+     *     "upsell": [],
+     *     "extension_attributes": {
+     *         "popular_together": [
+     *             "sku5",
+     *             "sku6"
+     *         ],
+     *         "similar_colors": [
+     *             "sku3x",
+     *             "sku4x"
+     *         ]
+     *     }
+     * }
+     *
+     * {@inheritDoc}
+     *
+     * @param string $sku Product SKU
+     * @param string $type Link Type: ['related', 'upsell', 'crosssell', 'extension_attributes', 'all']
+     *
+     * @return \Acquia\CommerceManager\Api\Data\TargetRuleProductsInterface $products
+     */
+    public function getRelatedProductsByType($sku, $type)
+    {
+        return $this->_getRelatedProductsByType($sku, $type);
+    }
+
     /**
      * {@inheritDoc}
      *
      * @param string $sku Product SKU
-     * @param string $type Link Type: ['related', 'upsell', 'crosssell', 'all']
+     * @param string $type Link Type: ['related', 'upsell', 'crosssell', 'extension', 'all']
      *
      * @return \Acquia\CommerceManager\Api\Data\TargetRuleProductsInterface $products
      */
-    public function getProductsByType($sku, $type)
+    protected function _getRelatedProductsByType($sku, $type)
     {
-        $result = [];
+        $this->relatedProducts = $this->targetRuleProductsFactory->create();
+        $this->relatedProducts->setSku($sku);
+        $this->relatedProducts->setType($type);
+
+        // Initialise with empty arrays (a convention of the ACM Commerce Connector)
+        $this->relatedProducts->setCrosssell([]);
+        $this->relatedProducts->setUpsell([]);
+        $this->relatedProducts->setRelated([]);
 
         if (!$this->targetModuleEnabled()) {
-            return (new TargetRuleProducts());
+            return $this->relatedProducts;
         }
 
-        $ruleClass = 'Magento\TargetRule\Model\Rule';
+        $ruleClass = $this->getTargetRuleClass();
+
         $types = [
-            'related' => constant($ruleClass . '::RELATED_PRODUCTS'),
-            'upsell' => constant($ruleClass . '::UP_SELLS'),
-            'crosssell' => constant($ruleClass . '::CROSS_SELLS'),
+            $this->relatedProducts::CROSS_SELL => $ruleClass::CROSS_SELLS,
+            $this->relatedProducts::UP_SELL => $ruleClass::UP_SELLS,
+            $this->relatedProducts::RELATED => $ruleClass::RELATED_PRODUCTS,
         ];
 
-        if (!isset($types[$type]) && ($type !== self::ALL_TYPES)) {
-            throw new \InvalidArgumentException(sprintf(
-                'Invalid relation type %s.',
-                $type
-            ));
-        }
-
-        $getTypes = ($type === self::ALL_TYPES) ? array_keys($types) : [$type];
-
-        $product = $this->productRepository->get($sku);
-        $excludeIds = [$product->getId()];
-
-        foreach ($getTypes as $type) {
-            $products = $this->getRuleProducts($types[$type], $product);
-            $typeProducts = [];
-            foreach ($products as $typeProduct) {
-                $typeProducts[] = $typeProduct->getSku();
+        // Only fetch relation types if we recognize the type.
+        if (isset($types[$type]) || ($type == self::ALL_TYPES)) {
+            $getTypes = [$type];
+            if ($type === self::ALL_TYPES)
+            {
+                $getTypes = array_keys($types);
             }
 
-            $result[$type] = $typeProducts;
+            $product = $this->productRepository->get($sku);
+
+            foreach ($getTypes as $typeKey) {
+                $products = $this->getRuleProducts($types[$typeKey], $product);
+                $typeProducts = [];
+                foreach ($products as $typeProduct) {
+                    $typeProducts[] = $typeProduct->getSku();
+                }
+                switch ($typeKey) {
+                    case $this->relatedProducts::CROSS_SELL:
+                        $this->relatedProducts->setCrosssell($typeProducts);
+                        break;
+                    case $this->relatedProducts::UP_SELL:
+                        $this->relatedProducts->setUpsell($typeProducts);
+                        break;
+                    case $this->relatedProducts::RELATED;
+                        $this->relatedProducts->setRelated($typeProducts);
+                        break;
+                    default:
+                }
+                $this->relatedProducts[$typeKey] = $typeProducts;
+            }
         }
 
-        return (new TargetRuleProducts(
-            (isset($result['crosssell'])) ? $result['crosssell'] : [],
-            (isset($result['related'])) ? $result['related'] : [],
-            (isset($result['upsell'])) ? $result['upsell'] : []
-        ));
+        // Seek extensions
+        // But please note extensions *must*
+        // plugin after $this->getRelatedProductsByType()
+        // or use the event observer (below)
+        $this->relatedProducts->getExtensionAttributes();
+
+        // Seek event-driven extensions
+        $this->relatedProducts->afterGetRelatedProductsByType();
+
+        return $this->relatedProducts;
     }
 
     /**
@@ -188,6 +275,24 @@ class TargetRuleRepository implements TargetRuleRepositoryInterface
     }
 
     /**
+     * getTargetRuleClass
+     *
+     * Get target rule class from the ObjectManager to prevent
+     * coupling to EE.
+     *
+     * @return \Magento\TargetRule\Model\Rule $ruleData
+     */
+    private function getTargetRuleClass()
+    {
+        if (!$this->targetRuleClass) {
+            $this->targetRuleClass = ObjectManager::getInstance()->get(
+                \Magento\TargetRule\Model\Rule::class
+            );
+        }
+        return ($this->targetRuleClass);
+    }
+
+    /**
      * getTargetRuleData
      *
      * Get target rule data helper from the ObjectManager to prevent
@@ -202,7 +307,6 @@ class TargetRuleRepository implements TargetRuleRepositoryInterface
                 \Magento\TargetRule\Helper\Data::class
             );
         }
-
         return ($this->targetRuleData);
     }
 
